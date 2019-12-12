0da71bd5719b690727c06a60e78be1df9470ca14
Daniel Cardona <daniel.cardona@eagleview.com>
2019-12-12 12:01:17 -0500

    Added support for greaterThan lessThan

--- KeypathAutolayout/NSLayoutConstraint+KeyPath.swift
+++ KeypathAutolayout/NSLayoutConstraint+KeyPath.swift
@@ -7,8 +7,25 @@
 //
 
 import UIKit
+// MARK: - Applying Constraints
+extension UIView {
+    func relativeTo(_ view: UIView, positioned constraints: [Constraint], priority: UILayoutPriority = .required) -> [NSLayoutConstraint] {
+        let constraints = Constraint.resolveConstraints(self, view, constraints: constraints)
+        constraints.forEach({ $0.priority = priority })
+        return constraints
+    }
 
+    func constrainedBy(_ constraints: [Constraint]) -> [NSLayoutConstraint] {
+        return Constraint.resolveConstraints(self, self, constraints: constraints)
+    }
+}
+
+// MARK: - Constraint
 public class Constraint {
+    public enum Relation {
+        case equal, greaterThanOrEqual, lessThanOrEqual
+    }
+
     typealias ConstraintBuilder = (UIView, UIView) -> NSLayoutConstraint
     public typealias Configuration = [Constraint]
     private var constraint: ConstraintBuilder
@@ -32,48 +49,95 @@ public class Constraint {
 
         return layoutConstraints
     }
+
+    public static func paired<Anchor, AnchorType>(_ keyPath: KeyPath<UIView, Anchor>,
+                                                  _ otherKeyPath: KeyPath<UIView, Anchor>? = nil,
+                                                  constraintRelation: Constraint.Relation = .equal,
+                                                  constant: CGFloat = 0,
+                                                  multiplier: CGFloat? = nil,
+                                                  priority: UILayoutPriority? = nil) -> Constraint where Anchor: NSLayoutAnchor<AnchorType> {
+
+        return Constraint { view, otherView in
+
+            var partialConstraint: NSLayoutConstraint
+            let otherKeyPath = otherKeyPath ?? keyPath
+
+            switch constraintRelation {
+            case .equal:
+                partialConstraint = view[keyPath: keyPath].constraint(equalTo: otherView[keyPath: otherKeyPath], constant: constant)
+            case .greaterThanOrEqual:
+                partialConstraint = view[keyPath: keyPath].constraint(greaterThanOrEqualTo: otherView[keyPath: otherKeyPath], constant: constant)
+            case .lessThanOrEqual:
+                partialConstraint = view[keyPath: keyPath].constraint(lessThanOrEqualTo: otherView[keyPath: otherKeyPath], constant: constant)
+            }
+
+            return NSLayoutConstraint.adjust(from: partialConstraint,
+                                             withMultiplier: multiplier,
+                                             priority: priority)
+
+        }
+
+    }
+
+    public static func unpaired<Anchor>(_ keyPath: KeyPath<UIView, Anchor>,
+                                        constraintRelation: Constraint.Relation = .equal,
+                                        constant: CGFloat = 0,
+                                        multiplier: CGFloat? = nil,
+                                        priority: UILayoutPriority? = nil) -> Constraint where Anchor: NSLayoutDimension {
+        return Constraint { view, _ in
+            var partialConstraint: NSLayoutConstraint
+
+            switch constraintRelation {
+            case .equal:
+                partialConstraint = view[keyPath: keyPath].constraint(equalToConstant: constant)
+            case .greaterThanOrEqual:
+                partialConstraint = view[keyPath: keyPath].constraint(greaterThanOrEqualToConstant: constant)
+            case .lessThanOrEqual:
+                partialConstraint = view[keyPath: keyPath].constraint(lessThanOrEqualToConstant: constant)
+            }
+
+            return NSLayoutConstraint.adjust(from: partialConstraint,
+                                             withMultiplier: multiplier,
+                                             priority: priority)
+        }
+    }
+
+    public static func equal<L, Axis>(_ to: KeyPath<UIView, L>, constant: CGFloat = 0.0) -> Constraint where L: NSLayoutAnchor<Axis> {
+        return paired(to, constraintRelation: .equal, constant: constant, multiplier: nil, priority: nil)
+    }
+
+    public static func equal<L>(_ from: KeyPath<UIView, L>, _ to: KeyPath<UIView, L>, constant: CGFloat = 0, multiplier: CGFloat? = nil) -> Constraint where L: NSLayoutDimension {
+        return paired(from, to, constraintRelation: .equal, constant: constant, multiplier: multiplier, priority: nil)
+    }
 }
 
+// MARK: - NSLayoutConstraint
 extension NSLayoutConstraint {
+
     public static func activating(_ constraints: [[NSLayoutConstraint]]) {
         let cons = constraints.flatMap({ $0 })
         NSLayoutConstraint.activate(cons)
     }
-}
 
-// MARK: - Constraint Primitives: Axis
-fileprivate func equal<L, Axis>(_ to: KeyPath<UIView, L>, constant: CGFloat = 0.0) -> Constraint where L: NSLayoutAnchor<Axis> {
-    return Constraint { view1, view2 in
-        view1[keyPath: to].constraint(equalTo: view2[keyPath: to], constant: constant)
-    }
-}
+    static func adjust(from constraint: NSLayoutConstraint,
+                       withMultiplier multiplier: CGFloat? = nil,
+                       priority: UILayoutPriority?) -> NSLayoutConstraint {
+        var constraint = constraint
+        if let multiplier = multiplier {
+            constraint = NSLayoutConstraint(item: constraint.firstItem as Any,
+                                            attribute: constraint.firstAttribute,
+                                            relatedBy: constraint.relation,
+                                            toItem: constraint.secondItem,
+                                            attribute: constraint.secondAttribute,
+                                            multiplier: multiplier,
+                                            constant: constraint.constant)
+        }
 
-fileprivate func equal<L, Axis>(_ from: KeyPath<UIView, L>, _ to: KeyPath<UIView, L>, constant: CGFloat = 0) -> Constraint where L: NSLayoutAnchor<Axis> {
-    return Constraint { view1, view2 in
-        view1[keyPath: from].constraint(equalTo: view2[keyPath: to], constant: constant)
-    }
-}
+        if let priority = priority {
+            constraint.priority = priority
+        }
 
-// MARK: - Constraint Primitives: Dimensions
-fileprivate func equalToConstant<L>(_ keyPath: KeyPath<UIView, L>, constant: CGFloat) -> Constraint where L: NSLayoutDimension {
-    return Constraint { view1, _ in
-        view1[keyPath: keyPath].constraint(equalToConstant: constant)
-    }
-}
-
-fileprivate func equal<L>(_ from: KeyPath<UIView, L>, _ to: KeyPath<UIView, L>, constant: CGFloat = 0, multiplier: CGFloat = 1) -> Constraint where L: NSLayoutDimension {
-    return Constraint { view1, view2 in
-        let dim1 = view1[keyPath: from]
-        let dim2 = view2[keyPath: to]
-        return dim1.constraint(equalTo: dim2, multiplier: multiplier, constant: constant)
-    }
-}
-
-fileprivate func equal<L>(_ from: KeyPath<UIView, L>, constant: CGFloat = 0, multiplier: CGFloat = 1) -> Constraint where L: NSLayoutDimension {
-    return Constraint { view1, view2 in
-        let dim1 = view1[keyPath: from]
-        let dim2 = view2[keyPath: from]
-        return dim1.constraint(equalTo: dim2, multiplier: multiplier, constant: constant)
+        return constraint
     }
 }
 
@@ -84,117 +148,98 @@ extension Constraint.Configuration {
     }
 
     public static func top(margin: CGFloat = 0) -> [Constraint] {
-        [equal(\.topAnchor, constant: margin)]
+        [.equal(\.topAnchor, constant: margin)]
     }
 
-
     public static func bottom(margin: CGFloat = 0) -> [Constraint] {
-        [equal(\.bottomAnchor, constant: -margin)]
+        [.equal(\.bottomAnchor, constant: -margin)]
     }
 
     public static func left(margin: CGFloat = 0) -> [Constraint] {
-        [equal(\.leftAnchor, constant: margin)]
+        [.equal(\.leftAnchor, constant: margin)]
     }
 
     public static func right(margin: CGFloat = 0) -> [Constraint] {
-        [equal(\.rightAnchor, constant: -margin)]
+        [.equal(\.rightAnchor, constant: -margin)]
     }
 
     // MARK: Safe Layout guide
     public static func safeTop(margin: CGFloat = 0) -> [Constraint] {
-        [equal(\.safeTopAnchor, constant: margin)]
+        [.equal(\.safeTopAnchor, constant: margin)]
     }
 
     public static func safeBottom(margin: CGFloat = 0) -> [Constraint] {
-        [equal(\.safeBottomAnchor, constant: margin)]
+        [.equal(\.safeBottomAnchor, constant: -margin)]
     }
 
     public static func safeLeft(margin: CGFloat = 0) -> [Constraint] {
-        [equal(\.safeLeftAnchor, constant: margin)]
+        [.equal(\.safeLeftAnchor, constant: margin)]
     }
 
     public static func safeRight(margin: CGFloat = 0) -> [Constraint] {
-        [equal(\.safeRightAnchor, constant: margin)]
+        [.equal(\.safeRightAnchor, constant: margin)]
     }
 
     public static func inset(by edgeInsets: UIEdgeInsets) -> [Constraint] {
         [
-            equal(\.topAnchor, constant: edgeInsets.top),
-            equal(\.rightAnchor, constant: -abs(edgeInsets.right)),
-            equal(\.leftAnchor, constant: abs(edgeInsets.left)),
-            equal(\.bottomAnchor, constant: -abs(edgeInsets.bottom)),
+            .equal(\.topAnchor, constant: edgeInsets.top),
+            .equal(\.rightAnchor, constant: -abs(edgeInsets.right)),
+            .equal(\.leftAnchor, constant: abs(edgeInsets.left)),
+            .equal(\.bottomAnchor, constant: -abs(edgeInsets.bottom))
         ]
     }
 
     public static var centered: [Constraint] {
-        [
-            equal(\.centerYAnchor),
-            equal(\.centerXAnchor),
-        ]
+        [.equal(\.centerYAnchor), .equal(\.centerXAnchor)]
     }
 
+    // MARK: Siblings
     public static func toLeft(spacing: CGFloat = 0) -> [Constraint] {
-        [equal(\.rightAnchor, \.leftAnchor, constant: -spacing)]
+        [.paired(\.rightAnchor, \.leftAnchor, constraintRelation: .equal, constant: -spacing, multiplier: nil, priority: nil)]
     }
 
     public static func toRight(spacing: CGFloat = 0) -> [Constraint] {
-        [equal(\.leftAnchor, \.rightAnchor, constant: spacing)]
+        [.paired(\.leftAnchor, \.rightAnchor, constraintRelation: .equal, constant: spacing)]
     }
 
     public static func below(spacing: CGFloat = 0) -> [Constraint] {
-        [equal(\.topAnchor, \.bottomAnchor, constant: spacing)]
+        [.paired(\.topAnchor, \.bottomAnchor, constraintRelation: .equal, constant: spacing)]
     }
 
     public static func above(spacing: CGFloat = 0) -> [Constraint] {
-        [equal(\.bottomAnchor, \.topAnchor, constant: -spacing)]
-    }
-
-    public static func equallySized() -> [Constraint] {
-        [equal(\.widthAnchor), equal(\.heightAnchor)]
+        [.paired(\.bottomAnchor, \.topAnchor, constraintRelation: .equal, constant: -spacing)]
     }
 
     public static func centerY(offset: CGFloat = 0) -> [Constraint] {
-        [equal(\.centerYAnchor, constant: offset)]
+        [.equal(\.centerYAnchor, constant: offset)]
     }
 
     public static func centerX(offset: CGFloat = 0) -> [Constraint] {
-        [equal(\.centerXAnchor, constant: offset)]
+        [.equal(\.centerXAnchor, constant: offset)]
     }
 
-    public static func equalWidth(constant: CGFloat = 0, multiplier: CGFloat = 1) -> [Constraint] {
-        [equal(\.widthAnchor, constant: constant, multiplier: multiplier)]
+    public static func equallySized() -> [Constraint] {
+        [.equal(\.widthAnchor), .equal(\.heightAnchor)]
     }
 
     public static func height(constant: CGFloat = 0, multiplier: CGFloat = 1) -> [Constraint] {
-        [equal(\.heightAnchor, constant: constant, multiplier: multiplier)]
+        [.paired(\.heightAnchor, constant: constant, multiplier: multiplier)]
     }
 
     public static func width(constant: CGFloat = 0, multiplier: CGFloat = 1) -> [Constraint] {
-        [equal(\.widthAnchor, constant: constant, multiplier: multiplier)]
+        [.paired(\.widthAnchor, constant: constant, multiplier: multiplier)]
     }
-    // MARK: - Self applied
+    // MARK: Self applied
     public static func constantHeight(_ height: CGFloat) -> [Constraint] {
-        [equalToConstant(\.heightAnchor, constant: height)]
+        [.unpaired(\.heightAnchor, constant: height)]
     }
 
     public static func constantWidth(_ constant: CGFloat = 0, multiplier: CGFloat = 1 ) -> [Constraint] {
-        [equalToConstant(\.widthAnchor, constant: constant)]
+        [.unpaired(\.widthAnchor, constant: constant)]
     }
 
     public static func aspectRatio(_ ratio: CGFloat) -> [Constraint] {
-        [equal(\.heightAnchor, \.widthAnchor, constant: 0, multiplier: ratio)]
-    }
-}
-
-
-// MARK: - Generic
-extension UIView {
-    func relativeTo(_ view: UIView, positioned constraints: [Constraint]) -> [NSLayoutConstraint] {
-        return Constraint.resolveConstraints(self, view, constraints: constraints)
-    }
-
-    func constrainedBy(_ constraints: [Constraint]) -> [NSLayoutConstraint] {
-        return Constraint.resolveConstraints(self, self, constraints: constraints)
+        [.equal(\.heightAnchor, \.widthAnchor, constant: 0, multiplier: ratio)]
     }
 }
 
@@ -204,7 +249,7 @@ extension UIView {
     }
 
     var safeBottomAnchor: NSLayoutYAxisAnchor {
-        return safeAreaLayoutGuide.topAnchor
+        return safeAreaLayoutGuide.bottomAnchor
     }
 
     var safeLeftAnchor: NSLayoutXAxisAnchor {
@@ -214,4 +259,3 @@ extension UIView {
         return safeAreaLayoutGuide.rightAnchor
     }
 }
-
